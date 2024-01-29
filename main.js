d1e3725195ad0b98ecfab571619a01ba1639b2ee
Alilool <120591534+Alilool@users.noreply.github.com>
2024-01-29 23:26:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 let body = document.getElementsByTagName("body")[0]
 let p = document.getElementsByTagName("p")
+let leftSection = document.getElementsByClassName("left-section")[0]
+let rightSection = document.getElementsByClassName("right-section")[0]
+let input = document.getElementsByTagName("input")
+
+
 let red = 255;
 let green = 255;
 let blue = 255;
 
+let red2 = 255;
+let green2 = 255;
+let blue2 = 255;
 function update_red(value) {
     red = value
     p[1].innerHTML = red
@@ -22,13 +30,64 @@ function update_blue(value) {
     changing_background()
 }
 
-function changing_background() {
-    console.log("input")
-    body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+function update_red_2(value) {
+    red2 = value
+    p[7].innerHTML = red2
+    changing_background()
+}
+
+function update_green_2(value) {
+    green2 = value
+    p[9].innerHTML = green2
+
+    changing_background()
+}
+
+function update_blue_2(value) {
+    blue2 = value
+    p[11].innerHTML = blue2
+    changing_background()
+}
+
+
+function changing_background() {  
+    console.log ("change")  
+    body.style.background = `linear-gradient(to right, rgba(${red}, ${green}, ${blue}, ${1}), rgba(${red2}, ${green2}, ${blue2}, ${1})`
     if (red < 100 && green < 100 && blue < 100) {
+        leftSection.style.color = `white`
+    } else {
+        leftSection.style.color = `Black`
+    }
+
+    if (red2 < 100 && green2 < 100 && blue2 < 100) {
+        rightSection.style.color = `white`
+    } else {
+        rightSection.style.color = `Black`
+    }
+
+    if (red < 100 && green < 100 && blue < 100 && red2 < 100 && green2 < 100 && blue2 < 100) {
         body.style.color = `white`
     } else {
         body.style.color = `Black`
     }
-}
 
+    let inputArray = Array.from(input);
+    let inputValues = inputArray.map(element => parseInt(element.value));
+
+    if (inputValues.some(value => value > 255)) {
+        alert("The limit is 255");
+
+        // Reset values of inputs exceeding 255 to 0
+        inputArray.forEach((element, index) => {
+            let value = parseInt(element.value);
+            if (value > 255) {
+                element.value = 0;
+
+                // Update associated <p> tag (assuming it's immediately after the input)
+                let associatedPTag = element.nextElementSibling;
+                if (associatedPTag && associatedPTag.tagName === "P") {
+                    associatedPTag.innerHTML = 0;
+                }
+            }
+        });
+    }}
